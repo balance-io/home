e4997d274d7a6a529db53f7b27a063b23fdbd798
Aleem Rehmtulla <aleem@rehmtulla.com>
2022-01-31 13:34:25 -0800

    centered div

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -6,7 +6,9 @@ export default function Hero() {
             {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
             <h1 className="text-black text-center mx-auto text-3xl pb-10">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
             <div>
+                <div className="flex justify-center">
                 <Image src="/Phone.svg" alt="balance" width="500" height="500" className="mx-auto" />
+                </div>
             </div>
             {/* </div> */}
         </section>
