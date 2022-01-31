184a9975513e5748efa6375249327d0240d8b942
Aleem Rehmtulla <aleem@rehmtulla.com>
2022-01-31 15:40:07 -0500

    Fixed

--- pages/components/features.js
+++ pages/components/features.js
@@ -1,25 +1,25 @@
 
 export default function Features() {
     return (
-        <div classname="flex flex-wrap overflow-hidden lg:-mx-2">
+        <div className="flex flex-wrap overflow-hidden lg:-mx-2">
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
 
             </div>
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div className="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
         </div>
--- pages/components/hero.js
+++ pages/components/hero.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section class="hero container max-w-screen-lg mx-auto pb-14 pt-10">
-            {/* <div className="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
-            <h1 class="text-black text-center mx-auto text-3xl pb-10">The <b class="font-medium">open source wallet</b> for the <b class="font-medium">Apple & Ethereum</b> communities</h1>
+        <section className="hero container max-w-screen-lg mx-auto pb-14 pt-10">
+            {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
+            <h1 className="text-black text-center mx-auto text-3xl pb-10">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
             <div>
-                <Image src="/Phone.svg" alt="balance" width="500" height="500" className="mx-auto" />
+                <Image src="/Phone.svg" alt="balance" width="500" height="500" classNameName="mx-auto" />
             </div>
             {/* </div> */}
         </section>
