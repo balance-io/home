1309ef0befb3cce382fd75d68ebdd67c4d06afec
Jake Adler <63482878+jakeradler@users.noreply.github.com>
2022-01-31 12:38:47 -0800

    Update features.js

--- pages/components/features.js
+++ pages/components/features.js
@@ -1,24 +1,25 @@
+
 export default function Features() {
     return (
-        <div class="flex flex-wrap overflow-hidden lg:-mx-2">
+        <div classname="flex flex-wrap overflow-hidden lg:-mx-2">
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
 
             </div>
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
-            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+            <div classname="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
             </div>
 
         </div>
