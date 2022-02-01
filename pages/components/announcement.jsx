03dd127e34a8356b2b9383822c5bf7a47b9526e3
Byeongjun Moon <byeongjun@bjmoon.io>
2022-01-31 16:31:53 -0800

    announcement fix

--- pages/components/announcement.jsx
+++ pages/components/announcement.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Announcement() {
     return (
         <div className="flex pt-5 p-5 bg-gray-200 w-screen justify-center">
-            <a className="text-gray-600 text-rg">Balance Launch @ ETHDenver. <a href="/" className="text-blue-500 hover:text-blue-700">Learn More ></a></a>
+            <a className="text-gray-600 text-rg">Balance Launch @ ETHDenver. <a href="/" className="text-blue-500 hover:text-blue-700">Learn More &gt;</a></a>
         </div>
 
     );
