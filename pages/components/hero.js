f5032af78679c7fe9e6563295038256d51942763
Byeongjun Moon <byeongjun@bjmoon.io>
2022-01-31 17:48:41 -0800

    merge fixes

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section className="hero container max-w-screen-lg mx-auto pt-10">
+        <><section className="hero container max-w-screen-lg mx-auto pt-10">
             {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
             <h1 className="text-black text-center mx-auto text-3xl pb-5">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
             <div>
@@ -11,14 +11,12 @@ export default function Hero() {
                 </div>
             </div>
             <div className="flex justify-center items-center ">
-                <div className="bg-black hover:bg-gray-700 py-4 px-6 mb-14 rounded-xl flex hover:cursor-pointer duration-300 ">
-                    <div className="w-8 h-8 pr-2 pt-0.5">
-                        <Image src="/Rings.svg" alt="balance" width="500" height="500" className="mx-auto" />
-                    </div>
-                    <button className=" text-white font-semibold"> Join The <span className='font-bold'>Crowdfund</span></button>
+                <div className="flex bg-black hover:bg-gray-700 py-4 px-6 mb-14 rounded-xl flex hover:cursor-pointer duration-300 space-x-2">
+                    <Image src="/Rings.svg" alt="balance" width="500" height="500" className="mx-auto" />
                 </div>
+                <button className=" text-white font-semibold"> Join The <b className='font-bold'>Crowdfund</b></button>
             </div>
-            <div className="pb-12" />
+        </div><div className="pb-12" /></>
         </section>
     );
 } 
