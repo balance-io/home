3b08f5aed78e902eee2a227332bf9e4eaa1e5c61
Byeongjun Moon <byeongjun@bjmoon.io>
2022-01-31 17:49:57 -0800

    final bug fix

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <><section className="hero container max-w-screen-lg mx-auto pt-10">
+        <section className="hero container max-w-screen-lg mx-auto pt-10">
             {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
             <h1 className="text-black text-center mx-auto text-3xl pb-5">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
             <div>
@@ -16,7 +16,7 @@ export default function Hero() {
                 </div>
                 <button className=" text-white font-semibold"> Join The <b className='font-bold'>Crowdfund</b></button>
             </div>
-        </div><div className="pb-12" /></>
+        <div className="pb-12" />
         </section>
     );
 } 
