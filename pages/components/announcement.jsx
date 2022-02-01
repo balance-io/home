547eef8ae0771abbc577b45ea3649de65cfc6cbd
Jake Adler <63482878+jakeradler@users.noreply.github.com>
2022-01-31 18:54:00 -0800

    h1

--- pages/components/announcement.jsx
+++ pages/components/announcement.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Announcement() {
     return (
         <div className="flex pt-5 p-5 bg-gray-200 w-screen justify-center">
-            <a className="text-gray-600 text-rg">Balance Crowdfunding -<a className="text-blue-500" href="https://ricburton.mirror.xyz/crowdfunds/0x9500b696F00AE82CA97d06379CF0A2b60B467040"> View Campaign </a>  </a>
+            <h1 className="text-gray-600 text-rg">Balance Crowdfunding -<a className="text-blue-500" href="https://ricburton.mirror.xyz/crowdfunds/0x9500b696F00AE82CA97d06379CF0A2b60B467040"> View Campaign </a>  </h1>
         </div >
 
     );
