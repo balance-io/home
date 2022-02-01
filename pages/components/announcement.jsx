77ae08fb13d5e7a1834ee2d94a9b70c264172bea
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-01-31 16:35:16 -0800

    <a> tag better

--- pages/components/announcement.jsx
+++ pages/components/announcement.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Announcement() {
     return (
         <div className="flex pt-5 p-5 bg-gray-200 w-screen justify-center">
-            <a className="text-gray-600 text-rg">Balance Launch @ ETHDenver. <a href="/" className="text-blue-500 hover:text-blue-700">Learn More &gt;</a></a>
+            <a className="text-gray-600 text-rg">Balance Launch @ ETHDenver.  <span className="text-blue-500 hover:cursor-pointer"> Learn More &gt; </span>  </a>
         </div>
 
     );
