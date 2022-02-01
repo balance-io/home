58882b8fc7e74d0f3f52753f3613dbbaf80c13ca
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-01-31 18:09:34 -0800

    footer v1

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -3,39 +3,75 @@ import Link from "next/link";
 
 function Footer() {
     return (
-        <section className=" pt-6 p-6 bg-[#131415] w-screen justify-center sticky top-0 z-50">
-            <h1 className="text-white text-center mx-auto text-2xl pb-10 ">How <b className="font-medium">you</b> can help <b className="font-medium">Us</b> before <b className="font-medium">we </b> launch the app </h1>
-            <div className=" space-x-12 items-center">
-
-
-            <div className="flex gap-3 justify-center">
+      <section className=" pt-6 p-6 bg-[#131415] w-screen justify-center sticky top-0 z-50 pr-20 pl-20">
+        <h1 className="text-white text-center mx-auto text-2xl pb-10 ">
+          How <b className="font-medium">you</b> can help{" "}
+          <b className="font-medium">Us</b> before{" "}
+          <b className="font-medium">we </b> launch the app{" "}
+        </h1>
+        <div className=" space-x-12 items-center">
+          <div className="flex gap-3 justify-center">
             <button className="bg-sky-500 text-white hover:bg-gray-700 py-4 px-6 mb-14 rounded-xl flex hover:cursor-pointer duration-300 ">
-               Follow on twitter
+              Follow on twitter
             </button>
 
             <button className="bg-indigo-600 text-white hover:bg-gray-700 py-4 px-6 mb-14 rounded-xl flex hover:cursor-pointer duration-300 ">
-                Join our discord
+              Join our discord
             </button>
 
             <button className="bg-stone-800 text-white hover:bg-gray-700 py-4 px-6 mb-14 rounded-xl flex hover:cursor-pointer duration-300 ">
-                Watch on github
+              Watch on github
             </button>
+          </div>
 
+          <div className="flex  justify-between ">
+            <div className="justify-end">
+              <h1 className="text-white justify-end text-4xl font-semibold pb-7">
+                Balance
+              </h1>
 
+             <h2 className="text-white text-xl">Balance Software, Inc.</h2>
+             <p className="text-slate-300">San Francisco, California</p>
+            <div className="pb-5" />
+             <h2 className="text-white text-xl">Want to visit us?</h2>
+                <p className="text-slate-300">We love having guests from <br />the Ethereum community in our <br /> Presidio Studio</p>
+              
             </div>
-
-            <div className="flex justify-center">
 
             <span className="justify-center">
-            <Image src="/assets/img/map.jpg" alt="balance" width="500" height="216" className="rounded-3xl " />
+              <Image
+                src="/assets/img/map.jpg"
+                alt="balance"
+                width="550"
+                height="266"
+                className="rounded-3xl "
+              />
             </span>
 
-            </div>
+            <div className="justify-end">
+              <h1 className="text-white justify-end text-3xl font-semibold">
+                Contact
+              </h1>
 
+              <p className="text-slate-300">
+                hello@balance.io <br />
+                Intercom Chat (SMS) <br />
+                Request a Call Back
+              </p>
 
+              <h1 className="text-white justify-end text-3xl font-semibold">
+                Media
+              </h1>
+
+              <p className="text-slate-300">
+                Blog <br />
+                Brand Kit <br />
+                Press contact
+              </p>
             </div>
-            
-        </section>
+          </div>
+        </div>
+      </section>
     );
 }
 
