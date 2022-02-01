4a201aed238a492a41f6d65ee5dd0f3f0e2aade7
Byeongjun Moon <byeongjun@bjmoon.io>
2022-01-31 16:30:02 -0800

    announcement bar

--- pages/components/announcement.jsx
+++ pages/components/announcement.jsx
@@ -1,8 +1,11 @@
 export default function Announcement() {
     return (
-        <div className="flex pt-3 p-3 bg-gray-300 w-screen justify-center">
-            <a className="text-gray-600 text-lg font-mono cursor-pointer">Balance Launch @ ETHDenver - <a className="text-blue-500 hover:text-blue-700">Learn More</a></a>
+        <div className="flex pt-5 p-5 bg-gray-200 w-screen justify-center">
+            <a className="text-gray-600 text-rg">Balance Launch @ ETHDenver. <a href="/" className="text-blue-500 hover:text-blue-700">Learn More ></a></a>
         </div>
 
     );
 }
+
+
+            
