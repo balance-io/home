3967fa37374b7f95d64ff648e9c9a8be0f3af0d7
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-01-31 19:35:16 -0800

    qv

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Hero() {
     return (
-        <section className="hero container max-w-screen-lg mx-auto pt-10 pr-10 pl-10">
+        <section className="hero container max-w-screen-lg mx-auto pt-10 pr-8 pl-8 md:pr-10 md:pl-10">
             {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
             <h1 className="text-black text-center mx-auto text-3xl pb-5">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
             <div>
@@ -11,7 +11,12 @@ export default function Hero() {
                 </div>
             </div>
             <div className="flex justify-center align-center">
-                <div className="flex bg-black text-white py-6 px-8 rounded-xl mb-14 font-semibold whitespace-nowrap space-x-2 hover:bg-gray-700 hover:cursor-pointer duration-300"><Image src="/Rings.svg" alt="balance" width="20" height="20" /> <p className="text-2xl font-normal">Join The <b className='font-bold'>Crowdfund</b></p></div>
+                <Link href="https://ricburton.mirror.xyz/crowdfunds/0x9500b696F00AE82CA97d06379CF0A2b60B467040" passHref>
+                <div className="flex bg-black text-white py-6 px-8 rounded-xl mb-14 font-semibold whitespace-nowrap space-x-2 hover:bg-gray-800 hover:cursor-pointer duration-300">
+                   <span className="hidden md:flex"> <Image src="/Rings.svg" alt="balance" width="20" height="20" className="" /> </span>
+                    <p className="text-2xl font-normal">Join The <b className='font-bold'>Crowdfund</b></p>
+                    </div>
+                </Link>
             </div>
       <div className="pb-12" />
         </section>
--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -3,13 +3,10 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <div className="flex pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
+        <div>
+        <div className="sm:flex hidden pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
             <div className="flex space-x-12 text-xl items-center		  ">
-                <Link href="">
-                    <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                        Wallet
-                    </a>
-                </Link>
+               
 
                 <Link href="https://discord.gg/V7j9MEuK8h">
                     <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
@@ -21,16 +18,41 @@ export default function Navigation() {
                 </div>
                 <Link href="">
                     <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                        Developers
+                        Blog
                     </a>
                 </Link>
 
-                <Link href="">
+                
+            </div>
+        </div>
+
+
+
+        <div className="sm:hidden flex  pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
+            <div className="flex space-x-12 text-xl items-center		  ">
+            <div>
+                    <Image src="/Balance.svg" alt="balance" width="60" height="60" className="" />
+                </div>
+
+            
+                <Link href="https://discord.gg/V7j9MEuK8h">
                     <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                        Store
+                        Discord
                     </a>
                 </Link>
+                
+                <Link href="">
+                    <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
+                        Blog
+                    </a>
+                </Link>
+
             </div>
+        </div>
+
+
+
+
         </div>
     );
 }
