4f60abd05a7a57b1298a9f9a5b7c336a961bcb8b
Jake Adler <63482878+jakeradler@users.noreply.github.com>
2022-02-02 19:44:28 -0800

    2.0

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -4,26 +4,15 @@ export default function Hero() {
     return (
         <section className="hero container max-w-screen-lg mx-auto pt-10 pr-8 pl-8 md:pr-10 md:pl-10">
             {/* <div classNameName="hero container flex flex-col pt-14 p-6 w-screen rounded-b-3xl justify-center "> */}
-            <h1 className="text-black text-center mx-auto text-3xl pb-5">The <b className="font-medium">open source wallet</b> for the <b className="font-medium">Apple & Ethereum</b> communities</h1>
-            <div>
-                <div className="flex place-content-center pr-14 -pb-5">
-                    <Image src="/Phone.svg" alt="balance" width="600" height="600" className="mx-auto" />
-                </div>
-            </div>
-            <div className="flex gap-2 justify-center align-center">
-                <Link href="https://ricburton.mirror.xyz/crowdfunds/0x9500b696F00AE82CA97d06379CF0A2b60B467040" passHref>
-                    <div className="flex bg-[#131415] text-white py-6 px-8 rounded-xl mb-14 font-semibold whitespace-nowrap space-x-2 hover:bg-gray-700 hover:cursor-pointer duration-300">
-                        <span className="hidden md:flex"> <Image src="/Rings.svg" alt="balance" width="30" height="30" className="" /> </span>
-                        <p className="text-2xl font-normal pl-2">Join The <b className='font-bold'>Crowdfund</b></p>
+            <h1 className="text-black text-center mx-auto text-4xl pb-5 tracking-wide">The open source wallet for the <br></br><b className="font-bold">Apple & Ethereum</b> communities</h1>
+
+            <div className="flex gap-2 justify-center align-center pt-10">
+                <Link href="https://discord.gg/sZtdVDrVGH">
+                    <div className="flex bg-gradient-to-br from-[#5ACDFF] to-[#1762E1] text-white py-2 px-2 rounded-xl mb-14 font-semibold whitespace-nowrap space-x-2 hover:bg-gray-700 hover:cursor-pointer duration-300">
+                        <span className="hidden md:flex"> <Image src="/Testflight2.svg" alt="balance" width="50" height="50" className="" /> </span>
+                        <p className="pl-2 text-sm antialiased">Download now on <br></br><b className='flex tracking-normal justify-center font-bold text-center text-2xl'>TestFlight</b></p>
                     </div>
                 </Link>
-
-                <a href="https://discord.gg/sZtdVDrVGH">
-                    <div className="flex bg-indigo-600 text-white py-6 px-8 pr-12 rounded-xl mb-14 font-semibold whitespace-nowrap space-x-2 hover:bg-gray-700 hover:cursor-pointer duration-300">
-                        <span className="hidden md:flex"> <Image src="/Discord.svg" alt="balance" width="30" height="30" className="" /> </span>
-                        <p className="text-2xl font-normal pl-2">Join our <b className='font-bold'>Discord</b></p>
-                    </div>
-                </a>
             </div>
             <div className="pb-12" />
         </section>
--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -3,60 +3,92 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <div>
-            <div className="sm:flex hidden pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
-                <div className="flex space-x-12 text-xl items-center		  ">
 
-
-                    <Link href="https://discord.gg/V7j9MEuK8h">
-                        <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                            Discord
-                        </a>
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 pt-10 mx-10">
+            <div class="container flex flex-wrap justify-between items-center mx-auto">
+                <a href="#" class="flex ml-2 ">
+                    <Link href="/">
+                        <Image src="/Logo.svg" alt="balance" width="30" height="30" className="cursor-pointer" />
                     </Link>
-                    <div>
-                        <Link href="/">
-                            <Image src="/Balance.svg" alt="balance" width="60" height="60" className="cursor-pointer" />
-                        </Link>
-                    </div>
-                    <Link href="https://ricburton.substack.com/">
-                        <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                            Blog
-                        </a>
-                    </Link>
-
-
+                </a>
+                <div class="flex items-center md:order-2">
+                </div>
+                <div class="hidden mr-12 justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+                    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-white text-xl text-black rounded md:bg-transparent md:p-0">Discord</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-white text-xl text-black rounded md:bg-transparent md:p-0">Blog</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-white text-xl text-black rounded md:bg-transparent md:p-0">GitHub</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </nav>
 
 
-
-            <div className="sm:hidden flex  pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
-                <div className="flex space-x-12 text-xl items-center		  ">
-                    <div>
-                        <Link href="/">
-                            <Image src="/Balance.svg" alt="balance" width="60" height="60" className="cursor-pointer" />
-                        </Link>
-                    </div>
-
-
-                    <Link href="https://discord.gg/V7j9MEuK8h">
-                        <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                            Discord
-                        </a>
-                    </Link>
-
-                    <Link href="">
-                        <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
-                            Blog
-                        </a>
-                    </Link>
-
-                </div>
-            </div>
-
-
-
-
-        </div>
     );
 }
+
+{/* // <div>
+        //     <div className="sm:flex hidden pt-6 p-6  w-screen sticky top-0 z-50">
+        //         <div className="justify-center top-0 z-5 w-screen">
+        //             <div className="flex space-x-12 text-xl items-center justify-center		  ">
+
+
+        //                 <Link href="https://discord.gg/V7j9MEuK8h">
+        //                     <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
+        //                         Discord
+        //                     </a>
+        //                 </Link>
+        //                 <div>
+        //                     <Link href="/">
+        //                         <Image src="/Logo.svg" alt="balance" width="60" height="60" className="cursor-pointer" />
+        //                     </Link>
+        //                 </div>
+        //                 <Link href="https://ricburton.substack.com/">
+        //                     <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
+        //                         Blog
+        //                     </a>
+        //                 </Link>
+        //             </div>
+
+        //         </div>
+        //         <div className="justify-left w-screen">
+        //             <button className="bg-blue-200 h-10 w-10">Hello</button>
+        //         </div>
+        //     </div>
+
+
+
+        //     <div className="sm:hidden flex  pt-6 p-6 bg-gray-100 w-screen justify-center sticky top-0 z-50">
+        //         <div className="flex space-x-12 text-xl items-center		  ">
+        //             <div>
+        //                 <Link href="/">
+        //                     <Image src="/Logo.svg" alt="balance" width="60" height="60" className="cursor-pointer" />
+        //                 </Link>
+        //             </div>
+
+
+        //             <Link href="https://discord.gg/V7j9MEuK8h">
+        //                 <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
+        //                     Discord
+        //                 </a>
+        //             </Link>
+
+        //             <Link href="">
+        //                 <a className="link link-underline link-underline-black text-black hover:text-slate-700 duration-300" target="_blank">
+        //                     Blog
+        //                 </a>
+        //             </Link>
+
+        //         </div>
+        //     </div>
+
+
+
+
+        // </div> */}
